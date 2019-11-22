460e5d2dad47699995b46d76729c7868eecfc736
Glen Datugan <Glen.datugan@gmail.com>
2019-11-22 18:13:23 +0000

    added stylesheet.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,66 +1,60 @@
 class GramsController < ApplicationController
- before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-  def index
-    @grams = Gram.all
-  end
-
-
-  def new
-    @gram = Gram.new
-  end
-
-
-  def create
-    @gram = current_user.grams.create(gram_params)
-      if @gram.valid?
-        redirect_to root_path
-        else
-          render :new, status: :unprocessable_entity
-      end
-  end
-
-
-  def show
+  def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
   end
-
-
-  def edit
-    @gram = Gram.find_by_id(params[:id])
-    return render_not_found if @gram.blank?
-    return render_not_found(:forbidden) if @gram.user != current_user
-  end
-
 
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
 
     if @gram.valid?
       redirect_to root_path
     else
-      render :edit, status: :unprocessable_entity
+      return render :edit, status: :unprocessable_entity
     end
   end
 
-
-  def destroy
-    @gram = Gram.find_by_id(params[:id])
-    return render_not_found if @gram.blank?
-    return render_not_found(:forbidden) if @gram.user != current_user
-    @gram.destroy
-    redirect_to root_path
+  def new
+    @gram = Gram.new
   end
 
+  def index
+    @grams = Gram.all
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    render_not_found if @gram.blank?
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    render_not_found if @gram.blank?
+  end
+
+  def create
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def gram_params
-    params.require(:gram).permit(:message, :id, :picture)
+    params.require(:gram).permit(:message, :picture)
   end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "grams#index"
-  resources :grams, only: [:new, :create, :show, :edit, :update, :destroy]
+    devise_for :users
+    root "grams#index"
+    resources :grams, only: [:new, :create, :show, :edit, :update, :destroy]
+    resources :comments, only: :create 
+  end
 end
